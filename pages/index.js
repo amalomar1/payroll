db652f1ec09ca1f95ff85839491dcf99338d4793
Amal Omar <amalomar1995@hotmail.com>
2023-03-09 16:09:30 +0000

    pages

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,52 @@
+import Link from "next/link"
 import React from "react"
 
-const Payroll=()=>{
-    return(
-       {employees.map(Data=> {
-                 return(
-                <>
-                </>
-                 )
-              })}
-    )
+
+const Payroll=({employees})=>{
+  return(
+    <>
+       {employees.map(data=> {
+         const bsicPay=data.Hoursworked*data.Basicpay
+
+         const totalAllowance= data.houseAllowance + data.conveyanceAllowance + data.childrenEducationAllowance
+
+         const totalReimbursement=data.fuelReimbursements +data.driverReimbursements
+         const totalDeduction=data.healthInsurance +data.taxIncome
+
+         const grossPay=basicPay + totalAllowance + totalReimbursement
+         const grossPay=basicPay + totalallowance + totalReimbursement
+
+         const netPay=grosspay-totalDeduction
+         return(
+           <>
+           <ul key={data.fullName}>
+           <Link href={`/${data._id}`} passHref>
+             <a>{data.fullName}</a>
+           </Link> 
+             <li>${grossPay}</li>
+             <li>${netPay}</li>
+           </ul>
+           </>
+           )
+           })}
+           </>
+  )
 }
+export async function getServerSideProps(){
+    
+    const data=await fetch(`http://localhost:3000/api/employees/`);
+    const employed=await data.json()
+
+    if(!employed){
+        return {
+            notFound:true
+        }
+    }
+    return{
+        props:{
+            employees:employed
+        }
+    }
+}
+
 export default Payroll
